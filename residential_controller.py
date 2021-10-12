15a6821ee10c9385b89d2fb13a93ff4785a085b0
Daniel Gallardo Ishii <dgallardo@edu.cegepgarneau.ca>
2021-10-12 14:03:49 -0400

    every object/class created

--- residential_controller.py
+++ residential_controller.py
@@ -1,18 +1,40 @@
+[elevatorID, floorRequestButtonID, callButtonID] = [1,1,1]
+
 class Column:
     def __init__(self, _id, _amountOfFloors, _amountOfElevators):
-
+        self.ID = _id
+        self.status = 'idle'
+        self.amountOfFloors = _amountOfFloors
+        self.amountOfElevators = _amountOfElevators
+        self.elevatorList = []
+        self.callButtonList = []
 
 class Elevator:
     def __init__(self, _id, _amountOfFloors):
+        self.ID = _id
+        self.status = 'off'
+        self._amountOfFloors = _amountOfFloors
+        self.currentFloor = 1
+        self.direction = 'idle'
+        self.door = Door
+        self.floorRequestButton = []
+        self.floorRequestList = []
 
 
 class CallButton:
     def __init__(self, _id, _floor, _direction):
-
+        self.ID = _id
+        self.status = 'off'
+        self.floor = _floor
+        self.direction = _direction
 
 class FloorRequestButton:
     def __init__(self, _id, _floor):
-
+        self.ID = _id
+        self.status = 'off'
+        self.floor = _floor
 
 class Door:
     def __init__(self, _id):
+        self.ID = _id
+        self.status = 'off'
